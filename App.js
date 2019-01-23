1f45e85df9a7b56bd876c7bb0001dfdfdd099091
SianLouisa <sianlouisa@hotmail.com>
2019-01-23 08:31:20 +0000

    update

--- App.js
+++ App.js
@@ -11,63 +11,58 @@
 
 import React, { Component } from 'react';
 
-import {
-  AppRegistry,
-} from 'react-native';
+import { AppRegistry } from 'react-native';
 
-import {
-  ViroVRSceneNavigator,
-  ViroARSceneNavigator,
-} from 'react-viro';
+import { ViroVRSceneNavigator, ViroARSceneNavigator } from 'react-viro';
 
 var createReactClass = require('create-react-class');
 
 /*
  * TODO: Add your API key below!!
  */
-var apiKey = "YOUR_API_KEY_HERE";
+var apiKey = 'EF29F8B0-C398-4B21-B392-56A481F3E192';
 
-var vrScenes = {
-    '360PhotoTour': require('./js/360PhotoTour/MainScene'),
-    'HumanBody': require('./js/HumanBody/MainScene'),
-    'ProductShowcase': require('./js/ProductShowcase/ProductShowcase'),
-    'ViroMediaPlayer': require('./js/ViroMediaPlayer/ViroTheatre'),
-    'ParticleEmitters': require('./js/ParticleEmitters/ViroParticleTemplates'),
-    'PhysicsSample': require('./js/PhysicsSample/BasicPhysicsSample'),
-}
+// var vrScenes = {
+//   '360PhotoTour': require('./js/360PhotoTour/MainScene'),
+//   HumanBody: require('./js/HumanBody/MainScene'),
+//   ProductShowcase: require('./js/ProductShowcase/ProductShowcase'),
+//   ViroMediaPlayer: require('./js/ViroMediaPlayer/ViroTheatre'),
+//   ParticleEmitters: require('./js/ParticleEmitters/ViroParticleTemplates'),
+//   PhysicsSample: require('./js/PhysicsSample/BasicPhysicsSample'),
+// };
 
 var arScenes = {
-  'ARSimpleSample': require('./js/ARSample/HelloWorldSceneAR.js'),
-  'ARPhysicsSample': require('./js/ARPhysicsSample/BasicPhysicsSample.js'),
-  'ARCarDemo' : require('./js/ARCarDemo/ARCarDemo.js'),
-  'ARPosterDemo' : require('./js/ARPosterDemo/ARPosterDemo.js'),
-  'BusinessCard' : require('./js/ARBusinessCard/BusinessCard.js'),
-}
+  // ARSimpleSample: require('./js/ARSample/HelloWorldSceneAR.js'),
+  ARPhysicsSample: require('./js/ARPhysicsSample/BasicPhysicsSample.js'),
+  // ARCarDemo: require('./js/ARCarDemo/ARCarDemo.js'),
+  // ARPosterDemo: require('./js/ARPosterDemo/ARPosterDemo.js'),
+  // BusinessCard: require('./js/ARBusinessCard/BusinessCard.js'),
+};
 
 var showARScene = true;
 
 var ViroCodeSamplesSceneNavigator = createReactClass({
   render: function() {
-
-    if (showARScene) {
-      return (
-        <ViroARSceneNavigator
-          initialScene={{
-            scene: arScenes['ARSimpleSample'],
-          }}
-          apiKey={apiKey} />
-        );
-    } else {
-      return (
-        <ViroVRSceneNavigator
-          initialScene={{
-            scene: vrScenes['360PhotoTour'],
-          }}
-          apiKey={apiKey} />
-      );
-
-    }
-  }
+    // if (showARScene) {
+    return (
+      <ViroARSceneNavigator
+        initialScene={{
+          scene: arScenes['ARPhysicsSample'],
+        }}
+        apiKey={apiKey}
+      />
+    );
+    // } else {
+    //   return (
+    //     <ViroVRSceneNavigator
+    //       initialScene={{
+    //         scene: vrScenes['360PhotoTour'],
+    //       }}
+    //       apiKey={apiKey}
+    //     />
+    //   );
+    // }
+  },
 });
 
 module.exports = ViroCodeSamplesSceneNavigator;
